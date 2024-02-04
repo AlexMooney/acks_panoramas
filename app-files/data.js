9c6bd97f7845f49d279277ff08fa951af97fd2ca
Alex Mooney <AlexMMooney@gmail.com>
2024-02-04 10:36:31 -0800

    Initial commit

---
+++ app-files/data.js
@@ -0,0 +1,80 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-gatehouse",
+      "name": "gatehouse",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 1.0166543102187386,
+        "pitch": 0.012880976911866071,
+        "fov": 1.4095469020700806
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-south_dungeon_entrance",
+      "name": "south_dungeon_entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 0.091294145488936,
+        "pitch": -0.11408865264797363,
+        "fov": 1.4095469020700806
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-south_dungeon_1",
+      "name": "south_dungeon_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 1.6817324590966827,
+        "pitch": -0.022046550685962174,
+        "fov": 1.4095469020700806
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
